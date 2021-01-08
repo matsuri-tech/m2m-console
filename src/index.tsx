2eddbf2233258d809e7beaef5d50792c7640cddb
myuon <ioi.joi.koi.loi@gmail.com>
2021-01-09 01:19:59 +0900

    feat: add m2m-users-app

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { Link, Route, BrowserRouter as Router, Switch } from "react-router-dom";
-import React from "react";
+import { mountTo } from "../apps/users/dist/app.js";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
@@ -10,15 +11,33 @@ const App = () => {
           <li>
             <Link to="/">Hello, World</Link>
           </li>
+          <li>
+            <Link to="/users">m2m-users</Link>
+          </li>
         </ul>
         <hr />
         <Switch>
           <Route exact path="/">
             <>hello, world</>
           </Route>
+          <Route path="/users">
+            <M2mUsersApp></M2mUsersApp>
+          </Route>
         </Switch>
       </div>
     </Router>
+  );
+};
+
+const M2mUsersApp = () => {
+  useEffect(() => {
+    mountTo("users");
+  }, []);
+
+  return (
+    <>
+      <div id="users"></div>
+    </>
   );
 };
 
