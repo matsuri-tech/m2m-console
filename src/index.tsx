f314fb18232adb33fda5bd2b79becb9bfa7bb432
myuon <ioi.joi.koi.loi@gmail.com>
2021-01-08 17:26:07 +0900

    feat: add files

---
+++ src/index.tsx
@@ -0,0 +1,25 @@
+import { Link, Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Hello, World</Link>
+          </li>
+        </ul>
+        <hr />
+        <Switch>
+          <Route exact path="/">
+            <>hello, world</>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("app"));
